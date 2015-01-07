46e07e3306d588523ebf3ad874d8dfa25bf78cc7
sf2050990 <sfox002@ucr.edu>
2015-01-06 20:03:07 -0800

    Signed-off-by: sf2050990 <sfox002@ucr.edu>

---
+++ Class/hello world/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Steve Fox
+ *
+ * Created on January 6, 2015, 6:05 PM
+ * Purpose: Our first program
+ */
+// System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+//Output Hello World
+    cout<< "Hello World"<< endl;
+    //Exit stage right
+    return 0;
+}
+
