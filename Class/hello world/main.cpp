4be8d8cd9a8d3dc34941f11e7b6400804c4c30d0
sf2050990 <sfox002@ucr.edu>
2015-01-08 18:41:00 -0800

    Signed-off-by: sf2050990 <sfox002@ucr.edu>

---
+++ Class/hello world/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Steve Fox
+ *
+ * Created on January 6, 2015, 6:05 PM
+ * Purpose: Our first program
+ * I am at github
+ */
+// System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototype
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+//Output Hello World
+    cout<< "Hello World"<< endl;
+    //Exit stage right
+    return 0;
+}
+
---
+++ Homework/Savage_9th_Chap1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 8, 2015, 6:25 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
